c0e0e129eb4cfb011e10e47c171f45d84f9b0bdb
“Edwin574” <owinoedwin574@gmail.com>
2026-06-23 22:04:23 +0300

    renaming shell script

--- test-docker.sh
+++ test-docker.sh
@@ -45,10 +45,12 @@ docker run taxi_ingest:v001 \
   --host=pg-database \
   --port=5432 \
   --db=nyc_taxi \
-  --table_name=yellow_taxi_trips \
+  --table_name=yellow_taxi_trips_1 \
   --url=${URL}
 
   ##corected
+
+URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
 
 docker run -it --network=pg-network taxi_ingest:v001 \
   --user=root \
