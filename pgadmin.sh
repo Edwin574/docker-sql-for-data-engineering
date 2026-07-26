245e85fa1fa08cabf020b353145c312f2727c19f
“Edwin574” <owinoedwin574@gmail.com>
2026-07-26 15:31:55 +0300

    Module 2: workflow ochestration

--- pgadmin.sh
+++ pgadmin.sh
@@ -1,7 +1,7 @@
 docker run -it \
   -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
   -e PGADMIN_DEFAULT_PASSWORD="root" \
-  -p 8080:80 \
+  -p 8082:80 \
   dpage/pgadmin4    
 
 docker network create pg-network
@@ -15,7 +15,7 @@ fe4ee8d09fb5f4f26d8487196a91d897f353d8ad21c1b13ea16d96e36de6209b
 docker run -it \
   -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
   -e PGADMIN_DEFAULT_PASSWORD="root" \
-  -p 8080:80 \
+  -p 8082:80 \
   --network=pg-network \
   --name pgadmin \
   dpage/pgadmin4  
